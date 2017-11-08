71e923e3b995d67e6e97867ec5445f0b55964686
Akhil Tayal <akhiltayal2818@gmail.com>
2017-11-08 13:28:32 +0530

    Major changes

--- index.js
+++ index.js
@@ -1,41 +1,79 @@
+// Setup basic express server
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+// var io = require('../..')(server);
+// New:
 var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 
-var port = Number(process.env.PORT || 3000);
-
-app.get('/', function(req,res){
-  res.send("Hello World !");
-})
-
-server.listen(port, ()=>{
-    console.log("Server running at port "+ port);
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
 });
 
-var nicknames = {} ;
+// Routing
+app.use(express.static(__dirname + '/public'));
 
-io.sockets.on('connection', function(socket){
-  socket.on('user message', function(msg){
-    socket.broadcast.emit('user message', socket.nickname, msg);
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+  console.log('connection');
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.emit('got message');
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
   });
 
-  socket.on('nickname', function(nick, fn){
-    if(nicknames[nick]){
-      fn(true);
-    }else{
-      fn(false);
-      nicknames[nick] = socket.nickname = nick;
-      socket.broadcast.emit('announcement', nick + ' connected');
-      io.socket.emit('nicknames', nicknames);
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+	console.log(username);
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
     }
-  });
-
-  socket.on('disconnect', function(){
-    if(!socket.nickname) return;
-
-    delete nicknames[socket.nickname];
-    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
-    socket.broadcast.emit('nicknames', nicknames);
   });
 });
